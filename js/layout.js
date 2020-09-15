78c9778cf014de68b9e3b313482cd5b4eae5cb1d
jayneeauza <jayneeauza@gmail.com>
2020-09-15 19:37:07 +0800

    up2

--- js/layout.js
+++ js/layout.js
@@ -2,12 +2,38 @@ $(document).ready(function() {
 	w3.includeHTML();
 });
 
-window.onscroll = function() {fncscroll()};
-
-function fncscroll() {
-	if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+$(window).scroll(function(evnt) {
+	var scroll = $(window).scrollTop();
+	if (scroll >= 200 && scroll <= 999) {
 		document.getElementById("divnav").style.top = "0";
-	} else {
+		$(".pd-row-2").animate({
+			opacity:'1'
+		});
+	}
+	else if (scroll >= 1000 && scroll <= 1499) {
+		document.getElementById("divnav").style.top = "0";
+		$(".pd-row-3 > .pd-clmn").animate({
+			top:'0px',
+			opacity:'1'
+		});
+	}
+	else if (scroll >= 1500 && scroll <= 2000) {
+		document.getElementById("divnav").style.top = "0";
+		$(".pd-row-4").animate({
+			opacity:'1'
+		});
+	}
+	else {
 		document.getElementById("divnav").style.top = "-80px";
 	}
-}
+});
+
+// window.onscroll = function() {fncscroll()};
+// function fncscroll() {
+// 	if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+// 		document.getElementById("divnav").style.top = "0";
+// 	}
+// 	else {
+// 		document.getElementById("divnav").style.top = "-80px";
+// 	}
+// }
